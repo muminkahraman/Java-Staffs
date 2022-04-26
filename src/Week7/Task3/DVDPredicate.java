086a3cdbfcead172d3c52bb4926800a216e257a9
Mumin Kahraman <39350247+muminkahraman@users.noreply.github.com>
2022-04-26 05:17:22 +0100

    Week 7 finished

---
+++ src/Week7/Task3/DVDPredicate.java
@@ -0,0 +1,20 @@
+package Week7.Task3;
+
+import java.util.function.Predicate;
+
+public class DVDPredicate implements Predicate<DVD> {
+    private char c1;
+    private char c2;
+
+    public DVDPredicate(char c1, char c2){
+        this.c1 = c1;
+        this.c2 = c2;
+    }
+
+    @Override
+    public boolean test(DVD dvd) {
+        char c = dvd.getTitle().charAt(0);
+        if (c>=c1 && c<c2) return true;
+        else return false;
+    }
+}
---
+++ src/Week7/Task3/Person.java
@@ -0,0 +1,45 @@
+package Week7.Task3;
+
+public class Person implements Person_comparator {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String firstName;
+    private String lastName;
+
+    public String getFullName(){
+        return (firstName + " " + lastName);
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + ' ' + lastName;
+    }
+
+    @Override
+    public int compareTo(Person p)
+    {
+        if (this.firstName.equals(p.firstName))
+        {
+            return this.lastName.compareTo(p.lastName);
+        }
+        else
+        {
+            return this.firstName.compareTo(p.firstName);
+        }
+    }
+}
